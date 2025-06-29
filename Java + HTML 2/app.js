301a1326e97bc84d5288289ba3f85a17283340c6
Jhonz777 <desurenovatio@gmail.com>
2025-06-29 18:46:40 -0300

    Java + HTML 2

---
+++ Java + HTML 2/app.js
@@ -0,0 +1,40 @@
+// Item 1
+function saudacao() {
+  console.log("Olá, mundo!");
+}
+saudacao();
+
+// Item 2
+function saudacaoNome(nome) {
+    console.log(`Olá, ${nome}!`);
+}
+saudacaoNome(nome);
+
+// Item 3
+function numeroRecebido(numero) { 
+  return numero * 2;
+}
+
+// Item 4
+function tresNumeros(n1, n2, n3) {
+    return (n1 + n2 + n3) / 3;
+}
+
+// Item 5
+function maiorNumero(a, b) {
+  if (a === b) {
+    return "Os dois números são iguais.";
+  }
+  return a > b ? a : b;
+}
+
+// Item 6
+function numeroXnumero(numeroX){
+    return numeroX * numeroX;
+}
+
+
+
+
+
+
