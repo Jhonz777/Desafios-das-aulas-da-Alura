0e02b92c2b2173b6bb702010df20127ccac7fb03
Jhonz777 <desurenovatio@gmail.com>
2025-07-02 16:24:03 -0300

    Java + HTML 4

---
+++ Java + HTML 4/app.js
@@ -0,0 +1,21 @@
+// Crie uma lista vazia, com o nome listaGenerica.
+let listaGenerica = [];
+
+// Crie uma lista de linguagens de programação chamada linguagensDeProgramacao com os seguintes elementos: 
+// 'JavaScript','C','C++', 'Kotlin' e 'Python'.
+let linguagensDeProgramacao = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
+
+// Adicione à lista linguagensDeProgramacao os seguintes elementos: 'Java', 'Ruby' e 'GoLang'.
+linguagensDeProgramacao.push('Java', 'Ruby', 'GoLang');
+
+// Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.
+let nomes1 = ['Guilherme', 'Monica', 'Alura'];
+console.log(nomes1[0]);
+
+/// Crie uma lista com 3 nomes e exiba no console apenas o segundo elemento.
+let nomes2 = ['Guilherme', 'Monica', 'Alura'];
+console.log(nomes2[1]);
+
+/// Crie uma lista com 3 nomes e exiba no console apenas o último elemento.
+let nomesLast = ['Guilherme', 'Monica', 'Alura'];
+console.log(nomesLast[nomesLast.length - 1]);
